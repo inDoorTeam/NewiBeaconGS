aec3defd9018ff66ad4adb58b419c976cac519b3
bingxyz <bingxyz1024@gmail.com>
2016-06-10 00:56:19 +0800

    新架構、UI

---
+++ app/src/main/java/gs/ibeacon/fcu/slideswipe/DLog.java
@@ -0,0 +1,14 @@
+package gs.ibeacon.fcu.slideswipe;
+
+import android.util.Log;
+
+/**
+ * Created by bing on 2016/6/9.
+ */
+public class DLog {
+    private static final String TAG = "BT_Chat";
+
+    public static void d(String msg){
+        Log.d(TAG, ">>> " + msg);
+    }
+}
